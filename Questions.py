99ed16da93c08903b41db9650c28db3ae0a3b8d2
sapirg50 <sapirg50@users.noreply.github.com>
2021-06-08 19:55:10 +0300

    in progress

--- Questions.py
+++ Questions.py
@@ -1,8 +1,9 @@
 class Question:
-    def __init__(self, question, options, question_type="close"):
+    def __init__(self, question, options, function, question_type="close"):
         self.question = question
         self.options = options
         self.type = question_type
+        self.function = function
 
     def ask(self):
         if self.type == "close":
@@ -12,7 +13,7 @@ class Question:
                     print(f'({i + 1}) {option}')
                 x = int(input())
                 if 1 <= x <= len(self.options):
-                    return x
+                    return self.options[x - 1]
                 else:
                     print("not valid")
                     exit(-1)
@@ -42,17 +43,18 @@ class Question:
                 exit(-1)
 
 
-q1 = Question("מהו המגדר שלך?", ["זכר", "נקבה", "לא רוצה לומר"])
-q2 = Question("מהו גילך (נא להזין מספר שלם)", None, "open")
-q3 = Question("מהו איזור מגורייך?", ["מרכז", "צפון", "דרום"])
-q4 = Question("כמה חשובה לך הרמה האקדמית?", [1, 5], "range")
-q5 = Question("כמה חשוב לך היחס האישי?", [1, 5], "range")
-q6 = Question("האם אתה מעדיף מכללה או אוניברסיטה?", ["מכללה", "אוניברסיטה", "לא משנה לי"])
-q7 = Question("דרג את החשיבות שלך עבור ההווי הסטודנטיאלי", [1, 5], "range")
-q8 = Question("בבחירת מוסד הלימודים, האם חשוב לך להישאר קרוב לבית?", ["כן", "לא"])
+q1 = Question("מהו המגדר שלך?", ["זכר", "נקבה", "לא רוצה לומר"], "replace")
+q2 = Question("מהו גילך (נא להזין מספר שלם)", None, "min max", "open")
+q3 = Question("מהו איזור מגורייך?", ["מרכז", "צפון", "דרום"], "replace")
+q4 = Question("כמה חשובה לך הרמה האקדמית?", [1, 5], "min max", "range")
+q5 = Question("כמה חשוב לך היחס האישי?", [1, 5], "min max", "range")
+q6 = Question("האם אתה מעדיף מכללה או אוניברסיטה?", ["מכללה", "אוניברסיטה", "לא משנה לי"], "replace")
+q7 = Question("דרג את החשיבות שלך עבור ההווי הסטודנטיאלי", [1, 5], "range", "min max")
+q8 = Question("בבחירת מוסד הלימודים, האם חשוב לך להישאר קרוב לבית?", ["כן", "לא"], "replace")
 q9 = Question("בבחירת מוסד הלימודים, האם היה חשוב לך להישאר קרוב לבית?",
-              ["מדעי המחשב", "פסיכולוגיה", "אומנות", "הנדסה", "משפטים", "סיעוד", "מנהל עסקים", "כלכלה/חשבונאות", "אחר"])
+              ["מדעי המחשב", "פסיכולוגיה", "אומנות", "הנדסה", "משפטים", "סיעוד", "מנהל עסקים", "כלכלה/חשבונאות", "אחר"],
+              "replace")
 q10 = Question("כמה אתה מעוניין לשלם עבור שכר לימוד בשנה?",
-               ["10,000 - 20,000", "20,000 - 30,000", "30,000 - 40,000", "40,000 ומעלה"])
+               ["10,000 - 20,000", "20,000 - 30,000", "30,000 - 40,000", "40,000 ומעלה"], "min max")
 
 question_list = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
--- main.py
+++ main.py
@@ -1,4 +1,46 @@
+import heapq
+
 import Questions
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances
+
+import xlrd
+import openpyxl
+
+# globals:
+payment_question = "כמה אתה מעוניין לשלם עבור שכר לימוד בשנה?"
+payment_answers = {"10,000 - 20,000": 1, "20,000 - 30,000": 2, "30,000 - 40,000": 3, "40,000 ומעלה": 4}
+final_question = 'היכן אתה לומד/למדת?'
+academic_institution = ""
+
+
+def get_data(user_lst):
+    df = pd.read_excel("train1.xlsx")
+    # save results and delete from DataFrame
+    results = df[final_question]
+    df = df.loc[:, df.columns != final_question]
+    # add current user
+    user_index = df.shape[0]
+    df.loc[df.shape[0]] = user_lst
+    # replace payment question to integer:
+    for ans in payment_answers:
+        df[payment_question].replace({ans: payment_answers[ans]}, inplace=True)
+    return user_index, results, df
+
+
+def min_max_normalization(x, column_name):
+    if x.max() != x.min():
+        users_data[column_name] = (x - x.min()) / (x.max() - x.min())
+    else:
+        users_data[column_name] = 1
+
+
+def replace_to(val_to_one, column_name):
+    rest = (np.unique(
+        np.delete(users_data[column_name].values, np.where(users_data[column_name].values == val_to_one)))).tolist()
+    users_data[column_name].replace(rest, 0, inplace=True)
+    users_data[column_name].replace({val_to_one: 1}, inplace=True)
 
 
 def user_questionnaire():
@@ -8,4 +50,21 @@ def user_questionnaire():
     return user_answer
 
 
-print(user_questionnaire())
+def keep_top_k(arr, k):
+    smallest = heapq.nlargest(k, arr)[-1]
+    # replace anything lower than the cut off with 0
+    arr[arr < smallest] = 0
+    return arr
+
+
+user_answers = user_questionnaire()
+index, users_results, users_data = get_data(user_answers)
+for i, q in enumerate(Questions.question_list):
+    if q.function == "replace":
+        # users_data.iloc[:, i]
+        replace_to(user_answers[i], users_data.columns[i])
+    else:
+        min_max_normalization(users_data.iloc[:, i], users_data.columns[i])
+user_similarity = 1 - pairwise_distances(users_data, metric='euclidean')
+
+# user_similarity = keep_top_k(np.array(user_similarity[index]), 2)
